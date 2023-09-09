7ba35e44e31241bde1f7c4d2b495a468f7354718
stephenkati <stephenkatuli88@gmail.com>
2023-09-09 11:43:17 +0300

    Add download Resume

---
+++ src/components/Resume.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BsDownload } from 'react-icons/bs'
+
+const RESUME_FILE = '/Stephen_Katuli`s_Resume.pdf';
+
+const Resume = () => {
+
+  const handleDownload = (url) => {
+    const filename = url.split("/").pop();
+    
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download", filename);
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
+
+  return (
+    <button
+      type='button'
+      onClick={() => handleDownload(RESUME_FILE)}
+      className="btn btn-secondary rounded-lg"
+    >
+      Download Resume <BsDownload />
+    </button>
+  )
+}
+
+export default Resume;
